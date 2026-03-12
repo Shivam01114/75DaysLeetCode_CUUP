2de94fbaead752cb1bf7266c711793978e24bda2
Shivam Singh <141226994+Shivam01114@users.noreply.github.com>
2026-03-12 07:19:12 +0530

    Time: 1719 ms (25.52%), Space: 19.7 MB (92.52%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,6 @@
 class Solution:
     def twoSum(self, nums, target):
-        num_map = {}  # value -> index
-        
-        for i, num in enumerate(nums):
-            complement = target - num
-            
-            # Check if complement already exists
-            if complement in num_map:
-                return [num_map[complement], i]
-            
-            # Store current number with index
-            num_map[num] = i
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
